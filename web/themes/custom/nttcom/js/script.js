c3178981406f9aaadeea88bbe12f5d5d3cd7a9ae
HUNGHV\hungh <hungh5614@gmail.com>
2023-01-05 21:47:05 +0700

    spot bottom

--- web/themes/custom/nttcom/js/script.js
+++ web/themes/custom/nttcom/js/script.js
@@ -6,3 +6,29 @@
     };
 })
 (jQuery, Drupal);
+
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 10,
+    responsiveClass:true,
+    nav: false,
+    responsive:{
+      0:{
+        items:2,
+        margin: 24
+      },
+      600:{
+        items:3,
+        margin: 30
+      },
+      1000:{
+        items:4,
+        margin: 40
+      }
+    }
+  });
+
+
+});
